8eed91c5dd4dcc428821e90b4d16c6d232f9139e
unknown <rishavmittal2903@gmail.com>
2020-06-25 15:09:14 +0530

    Updates

---
+++ precache-manifest.7bf775b1d8e70f38b18e448978d7db0d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5616e05ffcfefa18761905d99e815680",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "6508edfcd6d5a107b533",
+    "url": "/Portfolio/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "6d6972152cc820d8cd9f",
+    "url": "/Portfolio/static/css/main.e9212228.chunk.css"
+  },
+  {
+    "revision": "6508edfcd6d5a107b533",
+    "url": "/Portfolio/static/js/2.4b7f8574.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.4b7f8574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6972152cc820d8cd9f",
+    "url": "/Portfolio/static/js/main.9362f2ae.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "34a686d91e9c8b157e5d17f375ab425e",
+    "url": "/Portfolio/static/media/Calisto-MT-Bold.34a686d9.ttf"
+  },
+  {
+    "revision": "74abf7eb7351857ed68f7c9fe9216c21",
+    "url": "/Portfolio/static/media/Calisto-MT-Italic.74abf7eb.ttf"
+  },
+  {
+    "revision": "583e9c844f5d3cc9c7f3d408275356ff",
+    "url": "/Portfolio/static/media/Calisto-MT.583e9c84.ttf"
+  },
+  {
+    "revision": "a437db440af91825a818bd10af86b20a",
+    "url": "/Portfolio/static/media/about-me-bckg.a437db44.jpg"
+  },
+  {
+    "revision": "76a008515b98b0da922e77a68a8d6e58",
+    "url": "/Portfolio/static/media/aboutme.76a00851.jpg"
+  },
+  {
+    "revision": "ad9fcf833a1e00e2e346263e230574e2",
+    "url": "/Portfolio/static/media/banner.ad9fcf83.jpg"
+  },
+  {
+    "revision": "54c350720de9ec6c7d75023a099b05a5",
+    "url": "/Portfolio/static/media/book-bckg.54c35072.jpg"
+  },
+  {
+    "revision": "48c9ad2748e1d46614057a2af4798776",
+    "url": "/Portfolio/static/media/doc.48c9ad27.png"
+  },
+  {
+    "revision": "90ce921e16cd0f087fde196a03caca59",
+    "url": "/Portfolio/static/media/double-line.90ce921e.svg"
+  },
+  {
+    "revision": "251dbe331fcf016665087c9a9fe6a3a1",
+    "url": "/Portfolio/static/media/medi.251dbe33.jpg"
+  },
+  {
+    "revision": "a76698ae1575674eab2ca4f8df4ff319",
+    "url": "/Portfolio/static/media/quotes-bckg.a76698ae.jpg"
+  },
+  {
+    "revision": "a2e387d99f8ca6ba92a34a6d2ead1af5",
+    "url": "/Portfolio/static/media/security.a2e387d9.jpg"
+  },
+  {
+    "revision": "59c0cf8fe6dc252d432b702918872729",
+    "url": "/Portfolio/static/media/social-bckg.59c0cf8f.jpg"
+  },
+  {
+    "revision": "3573a577401e1ff60d6a2a4435298931",
+    "url": "/Portfolio/static/media/time.3573a577.jpg"
+  },
+  {
+    "revision": "51e16fab05907f5ac0fb86cd19d11286",
+    "url": "/Portfolio/static/media/top-banner-bckg.51e16fab.jpg"
+  }
+]);
